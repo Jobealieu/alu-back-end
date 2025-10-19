f384b6530a527fba6aeb4c3b6f74d6f08db4b754
Jobealieu <a.jobe@alustudent.com>
2025-10-19 14:08:09 -0700

    Add 3-dictionary_of_list_of_dictionaries.py script

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -8,29 +8,23 @@ import requests
 
 
 if __name__ == "__main__":
-    # Base URL for the API
     base_url = "https://jsonplaceholder.typicode.com"
-    
-    # Fetch all users
-    users_response = requests.get(f"{base_url}/users")
+
+    users_response = requests.get("{}/users".format(base_url))
     users = users_response.json()
-    
-    # Fetch all todos
-    todos_response = requests.get(f"{base_url}/todos")
+
+    todos_response = requests.get("{}/todos".format(base_url))
     todos = todos_response.json()
-    
-    # Create dictionary to store all employee tasks
+
     all_employees = {}
-    
-    # Process each user
+
     for user in users:
         user_id = str(user.get("id"))
         username = user.get("username")
-        
-        # Filter todos for this user
-        user_todos = [todo for todo in todos if todo.get("userId") == int(user_id)]
-        
-        # Create list of task dictionaries for this user
+
+        user_todos = [todo for todo in todos
+                      if todo.get("userId") == int(user_id)]
+
         task_list = []
         for todo in user_todos:
             task_dict = {
@@ -39,10 +33,8 @@ if __name__ == "__main__":
                 "completed": todo.get("completed")
             }
             task_list.append(task_dict)
-        
-        # Add to main dictionary
+
         all_employees[user_id] = task_list
-    
-    # Export to JSON file
+
     with open("todo_all_employees.json", "w") as json_file:
         json.dump(all_employees, json_file)
